e68a52c452b4060943a94229b44d1a2fcf9e141e
Raveena Bandara <raveenabandara801@gmail.com>
2022-07-22 10:34:11 +0530

    create the Backend server

---
+++ BACKEND/server.js
@@ -0,0 +1,33 @@
+const express = require("express"); //import dependencies
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+//configure the .env file
+require("dotenv").config();
+
+const URL = process.env.MONGODB_URL;
+
+//define connection
+mongoose.connect(URL, {});
+
+//Assign database connection for a constant variable
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB connection was successful"); //open connection for one time
+});
+
+const app = express();
+
+//Define a port for server
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port number ${PORT}`);
+});
+
+//use routes
