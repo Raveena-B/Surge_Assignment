c87fb15cbbdb0e8beecf5a6efa0ac2b605ff75f1
Raveena Bandara <raveenabandara801@gmail.com>
2022-07-24 11:31:49 +0530

    create seed file to add admin to the database directly

---
+++ BACKEND/data/seed.js
@@ -0,0 +1,16 @@
+const adminList = [
+  {
+    email: "raveenabandara801@gmail.com",
+    password: "rav123",
+  },
+  {
+    email: "kumarasirisahan@gmail.com",
+    password: "sahan123",
+  },
+  {
+    email: "tnirmalaadasooriya@gmail.com",
+    password: "thamali123",
+  },
+];
+
+module.exports = adminList;
